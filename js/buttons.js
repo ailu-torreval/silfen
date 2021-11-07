6581f4552c7a321a9ac7516c4588d5993d7b9622
ailin <aili0057@stud.kea.dk>
2021-11-07 20:57:18 +0100

    first js file, database fetched

---
+++ js/buttons.js
@@ -0,0 +1,33 @@
+fetch("http://magmadesign.dk/wp21/wp-json/wp/v2/bag?_embed")
+  .then((res) => res.json())
+
+  .then(gotData);
+
+function gotData(bags) {
+  bags.forEach(showBags);
+}
+
+function showBags(bag) {
+  console.log(bag);
+  const shCard = document.querySelector("#bag-template").content;
+  const clone = shCard.cloneNode(true);
+  clone.querySelector(".material").textContent = bag.material;
+  if (bag.material === "Recycled Nylon") {
+    clone.querySelector(".material").classList.add("opt1");
+  } else {
+    clone.querySelector(".material").classList.add("opt2");
+  }
+  clone.querySelector(".bag-img").src = bag.front_img.guid;
+
+  const parent = document.querySelector("#dynamic-grid");
+  parent.appendChild(clone);
+}
+
+// <section id="dynamic-grid">
+//     <template id="bag-template">
+//         <div class="img-card">
+//             <img class="bag-img" src="" alt="">
+//             <div class="opt1"></div>
+//         </div>
+//     </template>
+// </section>
